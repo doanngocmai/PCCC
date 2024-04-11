36a4a750fead06e83797f7441030f808fc8f6a4d
doanngocmai <mai@gmail.com>
2024-04-11 15:17:42 +0700

    change db

---
+++ PCCC.API/Controllers/WebAdmin/AdvertisingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PCCC.API.Controllers.WebAdmin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdvertisingController : ControllerBase
+    {
+    }
+}
